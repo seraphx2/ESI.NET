680ecfcf4fbcf74cd5851d9f2afaae8fece21701
tblin <tblin@users.noreply.github.com>
2018-11-22 11:40:53 +0100

    updated fields from esi

--- ESI.NET/Models/Corporation/Information.cs
+++ ESI.NET/Models/Corporation/Information.cs
@@ -1,4 +1,4 @@
-﻿using Newtonsoft.Json;
+using Newtonsoft.Json;
 
 namespace ESI.NET.Models.Corporation
 {
@@ -10,13 +10,31 @@ namespace ESI.NET.Models.Corporation
         [JsonProperty("ceo_id")]
         public int CeoId { get; set; }
 
-        [JsonProperty("corporation_name")]
-        public string Name { get; set; }
+        [JsonProperty("date_founded")]
+        public string DateFounded { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        [JsonProperty("home_station_id")]
+        public int HomeStationId { get; set; }
 
         [JsonProperty("member_count")]
         public int MemberCount { get; set; }
 
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("shares")]
+        public int Shares { get; set; }
+
+        [JsonProperty("tax_rate")]
+        public string TaxRate { get; set; }
+
         [JsonProperty("ticker")]
         public string Ticker { get; set; }
+
+        [JsonProperty("url")]
+        public string Url { get; set; }
     }
 }
