28fed138f149e1fd22a87aca83974f8b2204181f
David Naylor <naylor.b.david@gmail.com>
2022-12-06 21:16:16 +0200

    EsiRequest: avoid changing DefaultRequestHeaders as those are not thread-safe

--- ESI.NET/EsiRequest.cs
+++ ESI.NET/EsiRequest.cs
@@ -14,8 +14,6 @@ namespace ESI.NET
 
         public static async Task<EsiResponse<T>> Execute<T>(HttpClient client, EsiConfig config, RequestSecurity security, RequestMethod method, string endpoint, Dictionary<string, string> replacements = null, string[] parameters = null, object body = null, string token = null)
         {
-            client.DefaultRequestHeaders.Clear();
-
             var path = $"{method.ToString()}|{endpoint}";
 
             if (replacements != null)
@@ -24,55 +22,52 @@ namespace ESI.NET
 
             var url = $"{config.EsiUrl}latest{endpoint}?datasource={config.DataSource.ToEsiValue()}";
 
+            //Attach query string parameters
+            if (parameters != null)
+                url += $"&{string.Join("&", parameters)}";
+
+            HttpMethod httpMethod;
+            switch (method)
+            {
+                case RequestMethod.Delete:
+                    httpMethod = HttpMethod.Delete;
+                    break;
+                case RequestMethod.Get:
+                    httpMethod = HttpMethod.Get;
+                    break;
+                case RequestMethod.Post:
+                    httpMethod = HttpMethod.Post;
+                    break;
+                case RequestMethod.Put:
+                    httpMethod = HttpMethod.Put;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(method));
+            }
+
+            var request = new HttpRequestMessage(httpMethod, url);
+
             //Attach token to request header if this endpoint requires an authorized character
             if (security == RequestSecurity.Authenticated)
             {
                 if (token == null)
                     throw new ArgumentException("The request endpoint requires SSO authentication and a Token has not been provided.");
 
-                client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
             }
 
             if (ETag != null)
             {
-                client.DefaultRequestHeaders.Add("If-None-Match", $"\"{ETag}\"");
+                request.Headers.Add("If-None-Match", $"\"{ETag}\"");
                 ETag = null;
             }
 
-            //Attach query string parameters
-            if (parameters != null)
-                url += $"&{string.Join("&", parameters)}";
-
             //Serialize post body data
-            HttpContent postBody = null;
             if (body != null)
-                postBody = new StringContent(JsonConvert.SerializeObject(body), Encoding.UTF8, "application/json");
+                request.Content = new StringContent(JsonConvert.SerializeObject(body), Encoding.UTF8, "application/json");
 
-            //Get response from client based on request type
-            //This is also where body variables will be created and attached as necessary
-            HttpResponseMessage response = null;
-            switch (method)
-            {
-                case RequestMethod.Delete:
-                    response = await client.DeleteAsync(url).ConfigureAwait(false);
-                    break;
-
-                case RequestMethod.Get:
-                    response = await client.GetAsync(url).ConfigureAwait(false);
-                    break;
-
-                case RequestMethod.Post:
-                    response = await client.PostAsync(url, postBody).ConfigureAwait(false);
-                    break;
-
-                case RequestMethod.Put:
-                    response = await client.PutAsync(url, postBody).ConfigureAwait(false);
-                    break;
-            }
-            
             //Output final object
-            var obj = new EsiResponse<T>(response, path);
-            return obj;
+            return new EsiResponse<T>(await client.SendAsync(request).ConfigureAwait(false), path);
         }
 
         public enum RequestSecurity
